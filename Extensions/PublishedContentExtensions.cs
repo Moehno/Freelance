d4fcfd6b8422b13c272dec784f8c113c4a5c92d7
Moehno <kevin-schreiber@freenet.de>
2026-09-14 15:59:13 +0200

    Finished 4th Umbraco Tutorial Episode

---
+++ Extensions/PublishedContentExtensions.cs
@@ -0,0 +1,20 @@
+using Umbraco.Cms.Web.Common.PublishedModels;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace Freelancer.Extensions
+{
+    public static class PublishedContentExtensions
+    {
+        public static HomePage? GetHomePage(this IPublishedContent publishedContent)
+        {
+            return publishedContent.AncestorOrSelf<HomePage>();
+        }
+
+        public static SiteSettings? GetSiteSettings(this IPublishedContent publishedContent)
+        {
+            var homePage = GetHomePage(publishedContent);
+            if (homePage == null) return null;
+            return homePage.FirstChild<SiteSettings>();
+        }
+    }
+}
